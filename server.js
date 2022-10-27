101dea5cb8fa8716ca125cd25fa5cd363241d223
Angellyn218 <angellyncervantes@yahoo.com>
2022-10-27 11:18:30 -0700

    Added 404.html route

--- server.js
+++ server.js
@@ -27,6 +27,12 @@ app.get('/feedback', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
 );
 
+// GET Route for wildcard page
+app.get('*', (req, res) =>
+  // res.status(404).send("Sorry can't find that!")
+  res.sendFile(path.join(__dirname, '/public/pages/404.html'))
+);
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
